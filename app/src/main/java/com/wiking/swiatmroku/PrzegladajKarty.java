7ac07585b4fe1fb1a04c4c9d1f541f19ab72db8d
Wiking <Wiking@DESKTOP-BF1452S>
2018-01-07 18:37:43 +0100

    Generowanie listy radio button wraz z id z bazy danych

--- app/src/main/java/com/wiking/swiatmroku/PrzegladajKarty.java
+++ app/src/main/java/com/wiking/swiatmroku/PrzegladajKarty.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -28,5 +32,35 @@ public class PrzegladajKarty extends AppCompatActivity {
         KartaPostaci kartaPostaci=db.getKP(db.iloscKPwBazie()-1);
         Log.i("testy","IloscKart w bazie "+q);
         IloscKartWBazie.setText(q+" "+kartaPostaci.getImie());
+
+        RadioGroup rgp= (RadioGroup) findViewById(R.id.radioGrupsCardsView);
+        RadioGroup.LayoutParams rprms;
+
+        for(int i=q;i>=1;i--){
+            RadioButton radioButton = new RadioButton(this);
+            KartaPostaci kp = db.getKP(i);
+
+            radioButton.setId(i);
+            radioButton.setText(radioButton.getId()+". "+kp.getImie());
+            rprms= new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+            rgp.addView(radioButton, rprms);
+        }
+        /*
+        for (int row = 0; row < 1; row++) {
+            RadioGroup ll = new RadioGroup(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+
+            for (int i = 1; i <= q; i++) {
+                RadioButton rdbtn = new RadioButton(this);
+                rdbtn.setId((row * 2) + i);
+                rdbtn.setText("Radio " + rdbtn.getId());
+                ll.addView(rdbtn);
+            }
+            ((ViewGroup) findViewById(R.id.radioGrupsCardsView)).addView(ll);
+        }
+*/
+        //TODO dynamiczne ładowanie w scrollview i reliative layout i linear leyout ID NAZWA (Jjako BUTTON) i (usuń jako button) albo ID Radiobutton nazwa a na górze dwa klawisze otwórz lub usuń
     }
+
+
 }
